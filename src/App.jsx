6f313c86e9ca9270dcf857eb180c00d40e3aacc1
tahirasif31 <tahirasif31@gmail.com>
2024-01-08 12:18:43 -0800

    hero section completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+function Nav() {
+  return (
+    <nav>
+      <div className="nav-container">
+        <span className="logo">Whatsapp Communities</span>
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Faq</li>
+          <li>Blog</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
